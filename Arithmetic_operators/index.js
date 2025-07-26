4573858c18df450ff039f2f6d9fbe7274534bc92
Nightmer71 <nightmer@mailbox.unideb.hu>
2025-07-26 15:03:18 +0200

    Basic arithmetic operations in JavaScript

---
+++ Arithmetic_operators/index.js
@@ -0,0 +1,30 @@
+/* 
+
+arithmetic operators = 
+    operands (values, variables, etc...)
+    operators (+ - * /)
+    for example: 11 = x + 5;
+
+*/
+
+let students = 30;
+
+// students += 1; v. students++;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;  ** == ^
+// students %= 2;   Mennyi a maradék a students / 2 osztás után
+
+console.log(students);
+
+/* 
+operator presedence:
+    1. parenthesis ()
+    2. exponents (**)
+    3. multiplication, division, modulo
+    4. addition, substraction
+*/
+
+let result = 1 + 2 * 3 + 16;
+console.log(result);

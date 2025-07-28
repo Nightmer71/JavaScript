42ba2daf1451bf700775d6d71a8b300a433c1e94
Nightmer71 <nightmer@mailbox.unideb.hu>
2025-07-28 14:36:19 +0200

    Built-in JavaScript object called Math

---
+++ Math/index.js
@@ -0,0 +1,26 @@
+/* 
+
+Math:
+    Built-in object that provides a collection of properties and methods
+
+*/
+
+let x = 3;
+let y = 2;
+let z = 1;
+
+// z = Math.round(x); // Rounds to a whole number
+// z = Math.floor(x); // Rounds down to a whole number
+// z = Math.ceil(x); // Round up to a whole number
+// z = Math.trunc(x); // Levágja a tizedes pont mögötti számokat
+// z = Math.pow(x, y); // Raises x to the power of y
+// z = Math.sqrt(x); // Square root of x
+// z = Math.log(x); // x logaritmikus formája
+// z = Math.sin(x); // sin
+// z = Math.cos(x) // cosin
+// z = Math.abs(x) // absolute value of x
+
+// let max = Math.max(x, y, z); // Chooses the maximum value out of the given variables
+// let min = Math.min(x, y, z); // Chooses the minimum value out of the given variables
+
+console.log(z);

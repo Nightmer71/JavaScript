ddc6c5c918beed67178fda29c67a3c649d71cd82
Nightmer71 <totharon71@gmail.com>
2025-07-30 14:38:33 +0200

    Ternary operator in JavaScript

---
+++ Syntax/Ternary_operator/index.js
@@ -0,0 +1,14 @@
+/*
+
+ternary operator:
+    A shortcut to if{} and else{} statements.
+    Helps to assign a variable based on a condition.
+    condition ? codeIfTrue : codeIfFalse;
+
+*/ 
+
+/* An easy example
+let age = 21;
+let message = age >= 18 ? "You are an adult" : "You are a minor";
+console.log(message);
+*/

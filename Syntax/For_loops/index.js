c8726692d5166fd309d2ee07db8d4f32746f0a15
Nightmer71 <totharon71@gmail.com>
2025-08-13 14:48:46 +0200

    For loops in JavaScript

---
+++ Syntax/For_loops/index.js
@@ -0,0 +1,11 @@
+for(let i = 0; i <= 20; i++){
+    
+    if(i==13){
+        continue;
+
+        // break = breaks out of the for loop entirely
+    }
+    else{
+        console.log(i);
+    }
+}

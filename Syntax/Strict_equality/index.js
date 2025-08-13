70ddd8c8db55b99f1eb66afc40656d7a90181f47
Nightmer71 <totharon71@gmail.com>
2025-08-13 14:15:05 +0200

    Strict equality in JavaScript

---
+++ Syntax/Strict_equality/index.js
@@ -0,0 +1,16 @@
+/*
+    = assignment 
+    == comparison (values are equal)
+    === strict equality (value and datatype are equal)
+    != inequality 
+    !== strict inequality
+*/
+
+const PI = 3.14;
+
+if(PI === "3.14"){
+    console.log("That is PI");
+}
+else{
+    console.log("That is not PI")
+}

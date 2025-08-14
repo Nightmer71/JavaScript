c1160eb7149440f0c778c45c6ee32370324b6c7f
Nightmer71 <totharon71@gmail.com>
2025-08-14 15:08:23 +0200

    Functions in JavaScript

---
+++ Syntax/Functions/index.js
@@ -0,0 +1,25 @@
+/*
+
+Functions: A section of reusable code.
+
+*/
+
+/*
+function happyBirthday(username, age){
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log(`Happy birthday dear ${username}!`);
+    console.log("Happy birthday to you!");      
+    console.log(`You are ${age} years old!`);
+}
+
+happyBirthday("Nightmer", 20);
+*/
+
+function addNumbers(x, y){
+    let result = x + y;
+    return result;
+}
+
+let answer = addNumbers(2, 3);
+console.log(answer);

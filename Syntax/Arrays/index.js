ec3fee86ca20ae1c7993c7d909b2878c929c7348
Nightmer71 <totharon71@gmail.com>
2025-08-18 16:01:43 +0200

    Arrays in JavaScript

---
+++ Syntax/Arrays/index.js
@@ -0,0 +1,25 @@
+/*
+array = a variable like structure that can hold more than 1 value
+*/
+
+let fruits = ["apple", "orange", "banana"];
+
+// fruits.push("coconut"); Adds the element to the back of the array
+// fruits.pop(); Removes the element at the back of the array
+// fruits.unshift("mango"); Adds the element to the beginning of the array
+// fruits.shift(); Removes the element at the beginning of the array
+
+// let numOfFruits = fruits.length; Comes back with the lenght of the array (How many elements the array has inside)
+
+// let index = fruits.indexOf("apple"); Comes back with the index of the given element. If the element is not in the array it comes back with a -1 result
+
+// fruits.sort(); Sorts the elements in the array in alphabetical order
+// fruits.sort().reverse; Sorts the elements in the array in reverse alphabetical order
+
+/* Simple way to display the elements in the array individualy
+for (let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+*/
+
+console.log(fruits);
